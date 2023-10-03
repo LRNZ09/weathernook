6b36e5e15e96f658861d2686d1029fbe2793e511
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2023-10-03 22:25:55 +0200

    feat: call get current weather home page

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,5 +1,47 @@
+import {
+  Alert,
+  AlertTitle,
+  Button,
+  LinearProgress,
+  Typography,
+} from '@mui/material'
+import Page from '../../components/Page'
+import { useCallback } from 'react'
+import useGetCurrentWeather from '../../apis/weather/hooks/useGetCurrentWeather'
+
 const Home = () => {
-  return <h3>Welcome Home!</h3>
+  const queryText = 'Florence'
+  const { data, isLoading, isError, refetch } = useGetCurrentWeather({
+    queryText,
+  })
+
+  const handleRetry = useCallback(() => {
+    void refetch({ cancelRefetch: false })
+  }, [refetch])
+
+  if (isLoading) return <LinearProgress />
+  if (isError)
+    return (
+      <Page>
+        <Alert
+          severity='error'
+          action={
+            <Button color='inherit' size='small' onClick={handleRetry}>
+              RETRY
+            </Button>
+          }
+        >
+          <AlertTitle>An error occurred while loading the page</AlertTitle>
+          Please check your Internet connection and try again.
+        </Alert>
+      </Page>
+    )
+
+  return (
+    <Page>
+      <Typography>{JSON.stringify(data, null, 2)}</Typography>
+    </Page>
+  )
 }
 
 export default Home

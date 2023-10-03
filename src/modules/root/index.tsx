6f2a534285e0daafddb75286b34ab4b135210ad4
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2023-10-03 15:37:30 +0200

    chore: add router dev tools

--- src/modules/root/index.tsx
+++ src/modules/root/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import AppHeader from '../../components/AppHeader'
 
 const AppRoot = () => (
   <>
     <AppHeader />
-
-    <Outlet />
+    <Outlet /> {/* Start rendering router matches */}
+    <TanStackRouterDevtools />
   </>
 )
 

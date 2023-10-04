94731b2c71b0312053a486f0ec15c53c336a183e
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2023-10-05 00:59:01 +0200

    feat: add to speed util

---
+++ src/utils/toSpeed.test.ts
@@ -0,0 +1,26 @@
+import { test, fc } from '@fast-check/vitest'
+import { describe, expect } from 'vitest'
+import toSpeed, { measurementUnitSpeedMap } from './toSpeed'
+import measurementUnit from '../apis/weather/schemas/measurementUnit'
+
+const paramsArbitrary = {
+  value: fc.nat(),
+  measurementUnit: fc.constantFrom(...measurementUnit.options),
+}
+
+describe('toSpeed', () => {
+  test.prop(paramsArbitrary)('value', ({ value, measurementUnit }) => {
+    const res = toSpeed({ value, measurementUnit })
+
+    expect(res).toContain(value)
+  })
+
+  test.prop(paramsArbitrary)(
+    'measurementUnit',
+    ({ value, measurementUnit }) => {
+      const res = toSpeed({ value, measurementUnit })
+
+      expect(res).toContain(measurementUnitSpeedMap[measurementUnit])
+    },
+  )
+})
---
+++ src/utils/toSpeed.ts
@@ -0,0 +1,20 @@
+import measurementUnit, {
+  MeasurementUnit,
+} from '../apis/weather/schemas/measurementUnit'
+
+const measurementUnitSpeedMap = {
+  [measurementUnit.enum.imperial]: 'miles/hour',
+  [measurementUnit.enum.metric]: 'meter/sec',
+  [measurementUnit.enum.standard]: 'meter/sec',
+} as const satisfies Record<MeasurementUnit, string>
+
+const toSpeed = ({
+  value,
+  measurementUnit,
+}: {
+  value: number
+  measurementUnit: MeasurementUnit
+}) => `${value} ${measurementUnitSpeedMap[measurementUnit]}`
+
+export { measurementUnitSpeedMap }
+export default toSpeed

bf6bd00b441552283a15cd67f2a2cb89edb6eb4e
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2023-10-05 00:58:56 +0200

    feat: add to temperature util

---
+++ src/utils/toTemperature.test.ts
@@ -0,0 +1,26 @@
+import { test, fc } from '@fast-check/vitest'
+import { describe, expect } from 'vitest'
+import toTemperature, { measurementUnitTemperatureMap } from './toTemperature'
+import measurementUnit from '../apis/weather/schemas/measurementUnit'
+
+const paramsArbitrary = {
+  value: fc.nat(),
+  measurementUnit: fc.constantFrom(...measurementUnit.options),
+}
+
+describe('toTemperature', () => {
+  test.prop(paramsArbitrary)('value', ({ value, measurementUnit }) => {
+    const res = toTemperature({ value, measurementUnit })
+
+    expect(res).toContain(value)
+  })
+
+  test.prop(paramsArbitrary)(
+    'measurementUnit',
+    ({ value, measurementUnit }) => {
+      const res = toTemperature({ value, measurementUnit })
+
+      expect(res).toContain(measurementUnitTemperatureMap[measurementUnit])
+    },
+  )
+})
---
+++ src/utils/toTemperature.ts
@@ -0,0 +1,20 @@
+import measurementUnit, {
+  MeasurementUnit,
+} from '../apis/weather/schemas/measurementUnit'
+
+const measurementUnitTemperatureMap = {
+  [measurementUnit.enum.imperial]: '°F',
+  [measurementUnit.enum.metric]: '°C',
+  [measurementUnit.enum.standard]: '°K',
+} as const satisfies Record<MeasurementUnit, string>
+
+const toTemperature = ({
+  value,
+  measurementUnit,
+}: {
+  value: number
+  measurementUnit: MeasurementUnit
+}) => `${value} ${measurementUnitTemperatureMap[measurementUnit]}`
+
+export { measurementUnitTemperatureMap }
+export default toTemperature

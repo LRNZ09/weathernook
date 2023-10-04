b3fbab8237e7c71d6812f040aaa01408811f1b76
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2023-10-04 12:11:32 +0200

    feat: add weather detail card component

---
+++ src/components/WeatherDetailCard.tsx
@@ -0,0 +1,35 @@
+import {
+  Avatar,
+  Card,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from '@mui/material'
+import { SvgIconComponent } from '@mui/icons-material'
+
+export interface WeatherDetailCardProps {
+  label: string
+  value: string
+  icon: SvgIconComponent
+}
+
+const WeatherDetailCard = ({
+  label,
+  value,
+  icon: Icon,
+}: WeatherDetailCardProps) => {
+  return (
+    <Card>
+      <ListItem>
+        <ListItemAvatar>
+          <Avatar>
+            <Icon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={value} secondary={label} />
+      </ListItem>
+    </Card>
+  )
+}
+
+export default WeatherDetailCard

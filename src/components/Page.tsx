37f57a0b7f68df2da0aa5894fe62151b526ccbb6
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2023-10-05 00:58:16 +0200

    feat: measurement unit select in settings

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,6 +1,6 @@
 import { styled } from '@mui/material'
 import Column from './Column'
 
-const Page = styled(Column)(({ theme }) => ({ padding: theme.spacing(2) }))
+const Page = styled(Column)(({ theme }) => ({ padding: theme.spacing(4) }))
 
 export default Page

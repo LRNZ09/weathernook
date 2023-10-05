3ab4df745e4e469c78ea7675dbc63792ba516f0f
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2023-10-05 16:33:03 +0200

    style: add jsx a11y eslint package

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/strict-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:jsx-a11y/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',

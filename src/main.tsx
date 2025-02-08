c63d395bfb1a993bd0866d342ff426bb9afd62dc
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2025-02-08 20:17:07 +0100

    chore: migrate eslint prettier to biome

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,12 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.tsx'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')
+if (!rootElement) {
+  throw new Error('Root element not found!')
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
